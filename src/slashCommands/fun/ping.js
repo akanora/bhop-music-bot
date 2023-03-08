f5ebf5b2ff41138523afd137191524a6d42633b0
Nora <50810398+akanora@users.noreply.github.com>
2023-03-08 12:48:07 +0300

    Update Note
    The ping command has been updated to provide more detailed information about the bot's latency. Now, when the command is executed, it will return the round trip time and the bot's heartbeat. The previous version of the command only returned the bot's latency. The new version also includes minor improvements in the code structure and formatting to enhance readability. KEKW

--- src/slashCommands/fun/ping.js
+++ src/slashCommands/fun/ping.js
@@ -1,25 +1,23 @@
 module.exports = {
-    name: 'ping',
-    description: 'Very simple ping command',
-    usage: '', //OPTIONAL (for the help cmd)
-    examples: [], //OPTIONAL (for the help cmd)
-    dir: "fun",
-    cooldown: 1, // Cooldown in seconds, by default it's 2 seconds | OPTIONAL
-    permissions: [], // OPTIONAL
-    options: [
-        {
-            name: 'ping',
-            description: "Get the bot's latency",
-            type: 3, required: false,
-            choices: [ { name: "yes", value: 'true' }, { name: "no", value: 'false' } ]
-        }
-    ], // OPTIONAL, (/) command options ; read https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-structure
-    
-    run: async (client, interaction) => {
-        if(interaction.options.getString('ping') === 'true') {
-            interaction.reply({ content: `Hello world !\n> Bot's latency : **${Math.round(client.ws.ping)}ms**` });
-        } else {
-            interaction.reply({ content: 'Hello world !' });
-        }
+  name: 'ping',
+  description: 'Returns the round trip and heartbeat',
+  usage: '', //OPTIONAL (for the help cmd)
+  examples: [], //OPTIONAL (for the help cmd)
+  dir: 'fun',
+  cooldown: 1, // Cooldown in seconds, by default it's 2 seconds | OPTIONAL
+  permissions: [], // OPTIONAL
+
+  run: async (client, interaction) => {
+    const msg = await interaction.reply({ content: `Ping?`, fetchReply: true })
+
+    if (msg) {
+      const diff = msg.createdTimestamp - interaction.createdTimestamp
+      const ping = Math.round(client.ws.ping)
+      return interaction.editReply(
+        `The round trip took **${diff}ms** and the heartbeat being **${ping}ms**`
+      )
     }
+
+    return interaction.editReply('Failed to retrieve ping...')
+  },
 }
