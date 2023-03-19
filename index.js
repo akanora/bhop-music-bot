91a49ace06dc0b52d68e0cc2ee2e7cc133c20b47
Nora <50810398+akanora@users.noreply.github.com>
2023-03-19 19:47:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js')
-const { Player } = require('discord-player')
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
 const client = new Client({
-  allowedMentions: { parse: ['users', 'roles'] },
-  fetchAllMembers: true,
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
@@ -11,67 +13,35 @@ const client = new Client({
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
   ],
-})
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
+});
 
-//SET COLLECTION
-client.commandes = new Collection()
-client.slash = new Collection()
-client.aliases = new Collection()
-cooldowns = new Collection()
+const { Player } = require("discord-player");
 
-//SET UTILS
-client.logger = require('./src/utils/logger')
-client.color = require('./src/utils/color.js')
-
-//SET CONFIG
-client.config = require('./config')
-
-// LOAD THE 4 HANDLERS
-;['error', 'slashCommands', 'event'].forEach((file) => {
-  require(`./src/utils/handlers/${file}`)(client)
-})
-
-// this is the entrypoint for discord-player based application
 client.player = new Player(client, {
   ytdlOptions: {
-    quality: 'highestaudio',
+    quality: "highestaudio",
     smoothVolume: true,
     highWaterMark: 1 << 25,
   },
-})
+});
 
-client.player.events.on('playerStart', (queue, track) => {
-  queue.metadata.channel.send(`Started playing **${track.title}**!`)
-})
+const fs = require("fs");
+require("dotenv").config(); // remove this line if you are using replit
 
-client.player.events.on('playerError', (queue, error, track) => {
-  return queue.metadata.channel.send(
-    `There was an error with **${track.title}**!`
-  )
-})
+client.aliases = new Collection();
+client.slashCommands = new Collection();
 
-client.player.events.on('disconnect', (queue) => {
-  queue.metadata.channel.send(
-    'I have been **manually disconnected** from the **voice channel**'
-  )
-})
+module.exports = client;
 
-client.player.events.on('connectionError', (queue, error) => {
-  console.log(
-    `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
-  )
-})
+fs.readdirSync("./handlers").forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
+});
 
-client.player.events.on('emptyChannel', (queue, error) => {
-  queue.metadata.channel.send(
-    'I left the channel after **5 minutes** due to **channel inactivity**'
-  )
-})
-
-client.player.events.on('error', (queue, error) => {
-  console.log(
-    `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
-  )
-})
-
-client.login(client.config.token)
+client.login(process.env.TOKEN);
