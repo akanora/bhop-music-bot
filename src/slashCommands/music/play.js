d5146010898cb3d1493a85210edc4bb8fc1c1a37
Nora <50810398+akanora@users.noreply.github.com>
2023-03-19 15:37:11 +0300

    Update play.js

--- src/slashCommands/music/play.js
+++ src/slashCommands/music/play.js
@@ -1,65 +1,67 @@
+const { useMasterPlayer } = require("discord-player");
+
 module.exports = {
-  name: 'play',
-  description:
-    'Plays the specified song from YouTube, Soundcloud, Spotify etc.',
-  usage: '', //OPTIONAL (for the help cmd)
+  name: "play",
+  description: "Plays and enqueues track(s) of the query provided",
+  usage: "", //OPTIONAL (for the help cmd)
   examples: [], //OPTIONAL (for the help cmd)
-  dir: 'music',
+  dir: "music",
   cooldown: 1, // Cooldown in seconds, by default it's 2 seconds | OPTIONAL
   permissions: [], // OPTIONAL
   options: [
     {
-      name: 'search',
-      description: 'Plays and enqueues track(s) of the query provided.',
+      name: "query",
+      description: "Plays and enqueues track(s) of the query provided.",
       type: 3,
       required: true,
     },
   ],
 
   run: async (client, interaction) => {
-    const channel = interaction.member.voice.channel
-    if (!channel)
-      return interaction.reply('You are not connected to a voice channel!') // make sure we have a voice channel
-    const query = interaction.options.getString('search', true) // we need input/query to play
-    const results = await client.player.search(query)
-
+    const player = useMasterPlayer();
+    const query = interaction.options.getString("query", true);
+    const results = await player.search(query);
     if (!results.hasTracks())
-      return interaction.reply('No tracks were found for your query')
+      return interaction.reply({
+        content: `**No** tracks were found for your query`,
+        ephemeral: true,
+      });
 
-    await interaction.deferReply()
-    await interaction.editReply({
-      content: `⏳ | Loading ${
-        results.playlist ? 'a playlist...' : 'a track...'
-      }`,
-    })
+    await interaction.deferReply();
 
     try {
-      const { track } = await client.player.play(channel, results, {
-        nodeOptions: {
-          metadata: {
-            channel: interaction.channel,
-            client: interaction.guild.members.me,
-            requestedBy: interaction.user,
+      const res = await player.play(
+        interaction.member.voice.channel.id,
+        results,
+        {
+          nodeOptions: {
+            metadata: {
+              channel: interaction.channel,
+              client: interaction.guild.members.me,
+              requestedBy: interaction.user,
+            },
+            skipOnNoStream: true,
+            selfDeaf: true,
+            volume: 100,
+            leaveOnEmpty: true,
+            leaveOnEmptyCooldown: 300000,
+            leaveOnEnd: true,
+            leaveOnEndCooldown: 300000,
           },
-          skipOnNoStream: true,
-          selfDeaf: true,
-          volume: 100,
-          leaveOnEmpty: true,
-          leaveOnEmptyCooldown: 300000,
-          leaveOnEnd: true,
-          leaveOnEndCooldown: 300000,
-        },
-      })
-      await interaction.editReply({
+        }
+      );
+      return interaction.editReply({
         content: `Successfully enqueued${
-          track.playlist
-            ? ` **multiple tracks** from: **${track.playlist.title}**`
-            : `: **${track.title}**`
+          res.track.playlist
+            ? ` **track(s)** from: **${res.track.playlist.title}**`
+            : `: **${res.track.title}**`
         }`,
-      })
-    } catch (e) {
-      // let's return error if something failed
-      return interaction.followUp(`Something went wrong: ${e}`)
+      });
+    } catch (error) {
+      await interaction.editReply({
+        content: `An **error** has occurred`,
+      });
+      return console.log(error);
     }
   },
-}
+};
