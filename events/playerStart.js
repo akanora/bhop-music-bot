2f87e8da98d17758b199104950ac01d5c4ce8e6a
Nora <50810398+akanora@users.noreply.github.com>
2023-03-21 12:41:59 +0300

    Update playerStart.js

--- events/playerStart.js
+++ events/playerStart.js
@@ -1,5 +1,7 @@
 const client = require("..");
 
 client.player.events.on("playerStart", (queue, track) => {
-  queue.metadata.channel.send(`Started playing **${track.title}**!`);
+  queue.metadata.channel.send(
+    `Started playing **${track.author} - ${track.title}**!`
+  );
 });
