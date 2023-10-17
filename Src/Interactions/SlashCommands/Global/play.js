54459414284837c6a371ed63527788b0a902cb53
akanora <shironje@gmail.com>
2023-10-17 13:08:09 +0300

    Improved code readability and improved search result display

--- Src/Interactions/SlashCommands/Global/play.js
+++ Src/Interactions/SlashCommands/Global/play.js
@@ -18,20 +18,29 @@ module.exports = {
   ],
   autocomplete: async interaction => {
     const query = interaction.options.getString('query');
-    let returnData = [];
-    if (query) {
-      const result = await player.search(query);
-      if (result.playlist) {
-        const title =
-          result.playlist.title.length > 100
-            ? result.playlist.title.substring(0, 90) + '..(truncated)..'
-            : result.playlist.title;
-        returnData.push({ name: `${title} | Playlist`, value: query });
-      }
-      for (const track of result.tracks.slice(0, 6)) {
-        returnData.push({ name: track.title, value: track.url });
-      }
+    if (!query) return [];
+    const result = await player.search(query);
+
+    const returnData = [];
+    if (result.playlist) {
+      returnData.push({
+        name: 'Playlist | ' + result.playlist.title,
+        value: query,
+      });
     }
+
+    result.tracks.slice(0, 25).forEach(track => {
+      let name = `${track.title} | ${track.author ?? 'Unknown'} (${track.duration ?? 'n/a'})`;
+      if (name.length > 100) name = `${name.slice(0, 97)}...`;
+
+      let url = track.url;
+      if (url.length > 100) url = url.slice(0, 100);
+      return returnData.push({
+        name,
+        value: url,
+      });
+    });
+
     await interaction.respond(returnData);
   },
   run: async (client, interaction) => {
