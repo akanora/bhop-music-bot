96113f48cb29f820df063a4ebbd7a7071cf7baea
Nora <50810398+akanora@users.noreply.github.com>
2024-09-06 12:13:27 +0300

    discord-player-youtubei
    use discord-player-youtubei

--- bot.js
+++ bot.js
@@ -1,71 +1,71 @@
 (async () => {
-  const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
-  const { QuickDB } = require('quick.db');
-  const credentialManager = require('./Src/Credentials/Config');
-  const dirPath = __dirname;
-  const {
-    messageCommandsManager,
-    eventsManager,
-    buttonManager,
-    selectMenuManager,
-    modalFormsManager,
-    slashCommandsManager,
-  } = require('./Src/Structures/Managers/Export');
-  const { Player } = require('discord-player');
+    const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+    const youtubeAuth = "YOUR_FULL_COOKIE_STRING"
+    const { YoutubeiExtractor } = require("discord-player-youtubei")
+    const { QuickDB } = require('quick.db');
+    const { Player } = require('discord-player');
+    const credentialManager = require('./Src/Credentials/Config');
+    const dirPath = __dirname;
+    const {
+      messageCommandsManager,
+      eventsManager,
+      buttonManager,
+      selectMenuManager,
+      modalFormsManager,
+      slashCommandsManager,
+    } = require('./Src/Structures/Managers/Export');
+  
+    const botClient = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent, // Only for bots with message content intent access.
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildInvites,
+      ],
+      partials: [Partials.Channel],
+    });
 
-  const botClient = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.GuildPresences,
-      GatewayIntentBits.DirectMessages,
-      GatewayIntentBits.MessageContent, // Only for bots with message content intent access.
-      GatewayIntentBits.DirectMessageReactions,
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildMessageReactions,
-      GatewayIntentBits.GuildWebhooks,
-      GatewayIntentBits.GuildVoiceStates,
-      GatewayIntentBits.GuildInvites,
-    ],
-    partials: [Partials.Channel],
-  });
+    const player = new Player(botClient);
 
-  const player = new Player(botClient, {
-    useLegacyFFmpeg: false,
-    skipFFmpeg: false,
-    ytdlOptions: {
-        quality: 'highestaudio',
-        highWaterMark: 1 << 25,
-    }
-  });
+    await player.extractors.register(YoutubeiExtractor, {
+        authentication: youtubeAuth
+    });
 
-  await player.extractors.loadDefault();
-
-  exports.rootPath = dirPath;
-  exports.client = botClient;
-  exports.guildCooldownDB = new QuickDB({
-    filePath: `${dirPath}/guildCooldownDB.sqlite`,
-  });
-  exports.globalCooldownDB = new QuickDB({
-    filePath: `${dirPath}/globalCooldownDB.sqlite`,
-  });
-  exports.channelCooldownDB = new QuickDB({
-    filePath: `${dirPath}/channelCooldownDB.sqlite`,
-  });
-
-  botClient.messageCommands = new Collection();
-  botClient.messageCommandsAliases = new Collection();
-  botClient.events = new Collection();
-  botClient.buttonCommands = new Collection();
-  botClient.selectMenus = new Collection();
-  botClient.modalForms = new Collection();
-  botClient.slashCommands = new Collection();
-
-  await messageCommandsManager(botClient, dirPath);
-  await eventsManager(botClient, dirPath);
-  await buttonManager(botClient, dirPath);
-  await selectMenuManager(botClient, dirPath);
-  await modalFormsManager(botClient, dirPath);
-  await botClient.login(credentialManager.botToken);
-  await slashCommandsManager(botClient, dirPath);
-})();
+    await player.extractors.loadDefault((ext) => !['YouTubeExtractor'].includes(ext));
+  
+    exports.rootPath = dirPath;
+    exports.client = botClient;
+    exports.guildCooldownDB = new QuickDB({
+      filePath: `${dirPath}/guildCooldownDB.sqlite`,
+    });
+    exports.globalCooldownDB = new QuickDB({
+      filePath: `${dirPath}/globalCooldownDB.sqlite`,
+    });
+    exports.channelCooldownDB = new QuickDB({
+      filePath: `${dirPath}/channelCooldownDB.sqlite`,
+    });
+  
+    botClient.messageCommands = new Collection();
+    botClient.messageCommandsAliases = new Collection();
+    botClient.events = new Collection();
+    botClient.buttonCommands = new Collection();
+    botClient.selectMenus = new Collection();
+    botClient.modalForms = new Collection();
+    botClient.slashCommands = new Collection();
+  
+    await messageCommandsManager(botClient, dirPath);
+    await eventsManager(botClient, dirPath);
+    await buttonManager(botClient, dirPath);
+    await selectMenuManager(botClient, dirPath);
+    await modalFormsManager(botClient, dirPath);
+    await botClient.login(credentialManager.botToken);
+    await slashCommandsManager(botClient, dirPath);
+  })();
+  
